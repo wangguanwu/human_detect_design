9ce83d0a759e80d0a86979bcf65b9cae14dca1ea
wangguanwu <2531507093@qq.com>
2018-12-23 17:09:11 +0800

    format

--- cal_ave_bgtemperature.py
+++ cal_ave_bgtemperature.py
@@ -1,29 +1,32 @@
 import time
+from typing import List, Any
+
 import busio
 import board
 import adafruit_amg88xx
 import scipy
 import numpy as np
+
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 # we need th_bgframes frames  to calculate the average temperature of the bground
 th_bgframes = 300
-#the counter of the bgframes frames
+# the counter of the bgframes frames
 bgframe_cnt = 0
-all_bgframes = []
+all_bgframes: List[List[Any]] = []
 
 while True:
-   temp = []
-   for row in amg.pixels:
+    temp = []
+    for row in amg.pixels:
         # Pad to 1 decimal place
-       temp.append(row)
-    bgframe_cnt = bgframe_cnt+1
+        temp.append(row)
+    bgframe_cnt = bgframe_cnt + 1
     all_bgframes.append(temp)
     if bgframe_cnt >= th_bgframes:
-        print('next step')	
+        print('next step')
         print(len(all_bgframes))
         print(all_bgframes)
         break
 
-        
-  	
+
+
